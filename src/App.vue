550702d4eba67d0db45fa1877c6a50ec14ffaa22
Sam Bokai <sam.bokai@scout24.com>
2018-02-17 16:35:40 +0100

    Add webpack hot-reloading dev-server; Remove non-vue-decorator example component;

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Name: <input v-model="name" type="text">
+        <h1>Hello Decorator Component</h1>
+        <hello-decorator :name="name" :initialEnthusiasm="5" ></hello-decorator>
+    </div>
+</template>
+
+<script lang="ts">
+
+
+    import {Vue, Component, Prop, Provide} from "vue-property-decorator";
+    import HelloDecorator from './components/HelloDecorator.vue'
+
+    @Component({
+        components: {
+            HelloDecorator
+        }
+    })
+    export default class App extends Vue {
+        @Provide() name: string = "World";
+    }
+</script>
+
+<style scoped>
+
+</style>
